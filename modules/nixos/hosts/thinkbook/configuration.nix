b44e1b3614c1fbe15fa7fd95b7b3372a22c59412
muiga <muigask@gmail.com>
2026-04-27 22:08:02 +0300

    basic update

--- modules/nixos/hosts/thinkbook/configuration.nix
+++ modules/nixos/hosts/thinkbook/configuration.nix
@@ -7,21 +7,141 @@
       # import any other modules from here
       imports = [
         self.nixosModules.thinkbookHardware
-        self.nixosModules.niri
-        self.nixosModules.boot
-        self.nixosModules.networking
-        self.nixosModules.hardwareEnable
-        self.nixosModules.audio
-        self.nixosModules.printing
-        self.nixosModules.storage
-        self.nixosModules.virtualisation
-        self.nixosModules.programs
-        self.nixosModules.packages
-        self.nixosModules.fonts
-        self.nixosModules.nix
+        self.nixosModules.disko
       ];
 
-      # ...
+      # Bootloader.
+      boot = {
+        loader = {
+          systemd-boot = {
+            enable = true;
+            configurationLimit = 3;
+          };
+          efi.canTouchEfiVariables = true;
+          timeout = 1;
+        };
+        # Reduce swappiness
+        kernel.sysctl = {
+          "swappiness" = 10;
+        };
+      };
+
+      networking.hostName = "nixos"; # Define your hostname.
+      # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+      # Configure network proxy if necessary
+      # networking.proxy.default = "http://user:password@proxy:port/";
+      # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+      # Enable networking
+      networking.networkmanager.enable = true;
+
+      # Set your time zone.
+      time.timeZone = "Africa/Nairobi";
+
+      # Select internationalisation properties.
+      i18n.defaultLocale = "en_US.UTF-8";
+
+      # Enable the X11 windowing system.
+      services.xserver.enable = true;
+
+      # Enable the GNOME Desktop Environment.
+      services.displayManager.gdm.enable = true;
+      services.desktopManager.gnome.enable = true;
+
+      # Configure keymap in X11
+      services.xserver.xkb = {
+        layout = "us";
+        variant = "";
+      };
+
+      # Enable CUPS to print documents.
+      services.printing.enable = true;
+
+      # Enable sound with pipewire.
+      services.pulseaudio.enable = false;
+      security.rtkit.enable = true;
+      services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        # If you want to use JACK applications, uncomment this
+        #jack.enable = true;
+
+        # use the example session manager (no others are packaged yet so this is enabled by default,
+        # no need to redefine it in your config for now)
+        #media-session.enable = true;
+      };
+
+      # Enable touchpad support (enabled default in most desktopManager).
+      # services.xserver.libinput.enable = true;
+
+      # Define a user account. Don't forget to set a password with ‘passwd’.
+      users.users.muiga = {
+        isNormalUser = true;
+        description = "muiga";
+        extraGroups = [ "networkmanager" "wheel" ];
+        packages = with pkgs; [
+        #  thunderbird
+        ];
+      };
+
+      # Install firefox.
+      programs.firefox.enable = true;
+
+      # Allow unfree packages
+      nixpkgs.config.allowUnfree = true;
+
+      # List packages installed in system profile. To search, run:
+      # $ nix search wget
+      environment.systemPackages = with pkgs; [
+      #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      #  wget
+         git
+      ];
+
+      # Some programs need SUID wrappers, can be configured further or are
+      # started in user sessions.
+      # programs.mtr.enable = true;
+      # programs.gnupg.agent = {
+      #   enable = true;
+      #   enableSSHSupport = true;
+      # };
+
+      # List services that you want to enable:
+
+      # Enable the OpenSSH daemon.
+      # services.openssh.enable = true;
+
+      # Open ports in the firewall.
+      # networking.firewall.allowedTCPPorts = [ ... ];
+      # networking.firewall.allowedUDPPorts = [ ... ];
+      # Or disable the firewall altogether.
+      # networking.firewall.enable = false;
+      #
+      nix = {
+        settings = {
+            # auto-optimise-store = true;           # Optimise syslinks
+            experimental-features = [
+              "nix-command"
+              "flakes"
+            ];
+            max-jobs = "auto";
+            cores = 0;
+            use-xdg-base-directories = true;
+            http-connections = 50;
+        };
+      };
+
+      # This value determines the NixOS release from which the default
+      # settings for stateful data, like file locations and database versions
+      # on your system were taken. It‘s perfectly fine and recommended to leave
+      # this value at the release version of the first install of this system.
+      # Before changing this value read the documentation for this option
+      # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+      system.stateVersion = "25.11"; # Did you read the comment?
+
     };
 
 }
