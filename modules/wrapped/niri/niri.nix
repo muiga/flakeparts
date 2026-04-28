7d828dcf432cf5c8aa5f07c9e99f10e7d2952dbc
muiga <muigask@gmail.com>
2026-04-28 08:40:17 +0300

    niri 2

--- modules/wrapped/niri/niri.nix
+++ modules/wrapped/niri/niri.nix
@@ -1,39 +1,59 @@
-{ self, inputs, ... }:
+{ ... }:
 {
   flake.nixosModules.niri =
     { pkgs, ... }:
     {
       programs.niri = {
         enable = true;
-        package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNiri;
+        # package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNiri;
       };
+
+      environment.systemPackages = with pkgs; [
+        xwayland-satellite
+        rofi
+        swaybg
+        quickshell
+        hyprlock
+        hypridle
+        brightnessctl
+        #xdg-desktop-portal-gnome
+        #xdg-desktop-portal-gtk
+        #xdg-desktop-portal
+        gpu-screen-recorder
+        cliphist
+        noctalia-shell
+      ];
+
+      services.tuned.enable = true;
+      services.upower.enable = true;
+      services.udev.packages = [ pkgs.brightnessctl ];
     };
 
-  perSystem =
-    {
-      pkgs,
-      ...
-    }:
-    {
-      packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
-        inherit pkgs; # THIS PART IS VERY IMPORTAINT, I FORGOT IT IN THE VIDEO!!!
-        # settings = {
-        #   spawn-at-startup = [
-        #     (lib.getExe self'.packages.myNoctalia)
-        #   ];
+  # perSystem =
+  #   {
+  #     pkgs,
+  #     ...
+  #   }:
+  #   {
+  #     packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
+  #       inherit pkgs; # THIS PART IS VERY IMPORTAINT, I FORGOT IT IN THE VIDEO!!!
+  #       # settings = {
+  #       #   spawn-at-startup = [
+  #       #     (lib.getExe self'.packages.myNoctalia)
+  #       #   ];
 
-        #   xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
+  #       #   xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
 
-        #   input.keyboard.xkb.layout = "us,ua";
+  #       #   input.keyboard.xkb.layout = "us,ua";
 
-        #   layout.gaps = 5;
+  #       #   layout.gaps = 5;
 
-        #   binds = {
-        #     "Mod+Return".spawn-sh = lib.getExe pkgs.kitty;
-        #     "Mod+Q".close-window = null;
-        #     "Mod+S".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
-        #   };
-        # };
-      };
-    };
+  #       #   binds = {
+  #       #     "Mod+Return".spawn-sh = lib.getExe pkgs.kitty;
+  #       #     "Mod+Q".close-window = null;
+  #       #     "Mod+S".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
+  #       #   };
+  #       # };
+  #     };
+  #   };
 }
