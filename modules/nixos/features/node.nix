fb97cf05c179b7fa8c9b17d6bbc3c1af8c2f8962
muiga <muigask@gmail.com>
2026-07-22 08:35:37 +0300

    add node corepack 2

--- modules/nixos/features/node.nix
+++ modules/nixos/features/node.nix
@@ -8,22 +8,6 @@
       ];
 
       # Enable corepack and install specific versions
-      # environment.shellInit = ''
-      #   export COREPACK_HOME="$HOME/.cache/corepack"
-      #   export PATH="$HOME/.node/corepack/bin:$PATH"
-
-      #   # Create directories
-      #   mkdir -p "$HOME/.node/corepack/bin"
-      #   mkdir -p "$HOME/.cache/corepack"
-
-      #   # Enable corepack with install directory
-      #   corepack enable --install-directory "$HOME/.node/corepack/bin"
-
-      #   # Prepare versions
-      #   corepack prepare npm@latest --activate
-      #   corepack prepare pnpm@latest --activate
-      # '';
-
       systemd.user.services.corepack-setup = {
         description = "One-time corepack setup";
         wantedBy = [ "default.target" ];
