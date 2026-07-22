c7ee29a51312fa6904411aca430ce6acdeac306f
muiga <muigask@gmail.com>
2026-07-22 08:34:53 +0300

    add node corepack 1

--- modules/nixos/features/node.nix
+++ modules/nixos/features/node.nix
@@ -8,11 +8,43 @@
       ];
 
       # Enable corepack and install specific versions
+      # environment.shellInit = ''
+      #   export COREPACK_HOME="$HOME/.cache/corepack"
+      #   export PATH="$HOME/.node/corepack/bin:$PATH"
+
+      #   # Create directories
+      #   mkdir -p "$HOME/.node/corepack/bin"
+      #   mkdir -p "$HOME/.cache/corepack"
+
+      #   # Enable corepack with install directory
+      #   corepack enable --install-directory "$HOME/.node/corepack/bin"
+
+      #   # Prepare versions
+      #   corepack prepare npm@latest --activate
+      #   corepack prepare pnpm@latest --activate
+      # '';
+
+      systemd.user.services.corepack-setup = {
+        description = "One-time corepack setup";
+        wantedBy = [ "default.target" ];
+        unitConfig.ConditionPathExists = "!%h/.node/corepack/.setup-done";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = pkgs.writeShellScript "corepack-setup" ''
+            export COREPACK_HOME="$HOME/.cache/corepack"
+            mkdir -p "$HOME/.node/corepack/bin" "$HOME/.cache/corepack"
+            ${pkgs.nodejs}/bin/corepack enable --install-directory "$HOME/.node/corepack/bin"
+            ${pkgs.nodejs}/bin/corepack prepare npm@latest --activate
+            ${pkgs.nodejs}/bin/corepack prepare pnpm@latest --activate
+            touch "$HOME/.node/corepack/.setup-done"
+          '';
+        };
+      };
+
       environment.shellInit = ''
+        export COREPACK_HOME="$HOME/.cache/corepack"
         export PATH="$HOME/.node/corepack/bin:$PATH"
-        corepack enable
-        corepack prepare npm@latest --activate
-        corepack prepare pnpm@latest --activate
       '';
+
     };
 }
